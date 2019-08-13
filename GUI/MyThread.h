dc55f0abc2a96bbab861b60ff3f053a2e80eeb4c
cic12 <cic12@ic.ac.uk>
2019-08-13 12:42:19 +0200

    sim config

--- GUI/MyThread.h
+++ GUI/MyThread.h
@@ -13,8 +13,8 @@
 using namespace std;
 
 struct testParams {
-	bool Sim = 0, Motor = 1, aiSim = 0, tauEst = 1, Mode = 1, Exo = 1; // Exo
-	//bool Sim = 1, Motor = 0, aiSim = 1, tauEst = 1, Mode = 1, Exo = 1; // Sim
+	//bool Sim = 0, Motor = 1, aiSim = 0, tauEst = 1, Mode = 1, Exo = 1; // Exo
+	bool Sim = 1, Motor = 0, aiSim = 1, tauEst = 1, Mode = 1, Exo = 1; // Sim
 };
 
 struct mpcParams {
