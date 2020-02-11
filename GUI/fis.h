558dd3203363f428495673f1c1caeaf0453655fb
cic12 <cic12@ic.ac.uk>
2020-02-11 17:18:46 +0000

    test config chris

--- GUI/fis.h
+++ GUI/fis.h
@@ -5,7 +5,7 @@
 
 struct fisParams {
 	double b1 = 0.181042528144174, b2 = 206.216871616737, b3 = -90.5225975988012; // from emg_torque_fit.m
-	double pA = 1, pR = 1;
+	double pA = 0.8, pR = 1;
 	double s0 = 0.9, s1 = 0.8;
 
 	double eZ_sig = 0.1, eZ_c = 0;
