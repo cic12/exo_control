bd33529f95546577c38494d45e2b5f55da2bfa51
cic12 <cic12@ic.ac.uk>
2020-03-11 23:13:23 +0000

    QtCharts ref removed

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -1,13 +1,6 @@
 #include "GUI.h"
 #include <QtWidgets/QApplication>
 
-#include <QtCharts/QChartView>
-#include <QtCharts/QChart>
-#include <QtCharts/QValueAxis>
-#include <QtCharts/QLineSeries>
-
-using namespace QtCharts;
-
 int main(int argc, char *argv[]) {
 	SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
 	QApplication a(argc, argv);
