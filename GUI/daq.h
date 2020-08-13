4e2a6f00066aa04142f53f8b216319e67f174ba0
cic12 <cic12@ic.ac.uk>
2020-08-13 22:28:34 +0100

    daqmx2 commented.

--- GUI/daq.h
+++ GUI/daq.h
@@ -2,22 +2,22 @@
 #define DAQ_H_
 
 #include <fstream>
-#include "NIDAQmx.h"
+//#include "NIDAQmx.h"
 
 #define DAQmxErrChk(functionCall) if( DAQmxFailed(error=(functionCall)) ) goto Error; else
 
 using namespace std;
 
-extern float64 AIdata[2], AIm[2];
-extern int32 error;
-extern char errBuff[2048];
-extern float64 AOdata[2];
+//extern float64 AIdata[2], AIm[2];
+//extern int32 error;
+//extern char errBuff[2048];
+//extern float64 AOdata[2];
+//
+//TaskHandle DAQmxAIinit(int32 error, char &errBuff, TaskHandle AItaskHandle, int AIsamplingRate);
+//TaskHandle DAQmxAOinit(float64 &AOdata, int32 error, char &errBuff, TaskHandle AOtaskHandle);
+//TaskHandle DAQmxAstart(int32 error, char &errBuff, TaskHandle taskHandle);
 
-TaskHandle DAQmxAIinit(int32 error, char &errBuff, TaskHandle AItaskHandle, int AIsamplingRate);
-TaskHandle DAQmxAOinit(float64 &AOdata, int32 error, char &errBuff, TaskHandle AOtaskHandle);
-TaskHandle DAQmxAstart(int32 error, char &errBuff, TaskHandle taskHandle);
-
-int32 CVICALLBACK EveryNCallback(TaskHandle taskHandle, int32 everyNsamplesEventType, uInt32 nSamples, void* callbackData);
+//int32 CVICALLBACK EveryNCallback(TaskHandle taskHandle, int32 everyNsamplesEventType, uInt32 nSamples, void* callbackData);
 
 struct filter_para {
 	double x[3][4] = {};
